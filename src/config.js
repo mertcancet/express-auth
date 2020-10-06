e441cf298f8804efffbc1eff94da0fc5ad76c337
mertcancet <61883804+mertcancet@users.noreply.github.com>
2020-10-06 21:44:03 +0300

    add jwt

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
   version: 'v1',
+  jwtSecret: 'secretKey',
 }
--- src/routes/auth/auth.router.js
+++ src/routes/auth/auth.router.js
@@ -1,10 +1,36 @@
 import express from 'express'
 import config from 'config'
+import jwt from 'jsonwebtoken'
+
+const users = [
+  {
+    _id: 'user123',
+    firstnName: 'mertcan',
+    lastName: 'cetinkaya',
+    email: 'mertcan@mertcan.com',
+    password: '1234',
+  },
+]
 
 const route = () => {
   const router = new express.Router()
   router.route('/login').post((req, res) => {
-    res.send('ok')
+    const { email, password } = req.body
+
+    const user = users.find((user) => user.email === email)
+    if (!user) {
+      res.send({
+        status: false,
+        message: 'Böyle bir email adresi yok!!!',
+      })
+    } else {
+      if (user.password === password) {
+        const token = jwt.sign({ userId: user._id }, config.jwtSecret)
+        res.send({ status: true, token: token })
+      } else {
+        res.send({ status: false, message: 'hatalı sifre' })
+      }
+    }
   })
   return router
 }
