2b8c7b81cc02a16251edd5d1d670098e0da98c17
mertcancet <61883804+mertcancet@users.noreply.github.com>
2020-10-06 17:48:32 +0300

    express-routing

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import AppRouter from './routes'
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+AppRouter(app)
+app.get('/', (req, res) => {
+  res.send('rest adpi')
+})
+
+app.listen(3300, () => {
+  console.log('server is on 3300')
+})
